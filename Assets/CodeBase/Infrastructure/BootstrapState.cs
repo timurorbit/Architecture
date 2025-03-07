8f7abefbc4ed4433f7379963065bd6f936292c2e
Timur <t.shamilov.hollow@gmail.com>
2025-03-07 14:59:19 +0100

    StateMachine refactor

--- Assets/CodeBase/Infrastructure/BootstrapState.cs
+++ Assets/CodeBase/Infrastructure/BootstrapState.cs
@@ -24,7 +24,7 @@ namespace CodeBase.Infrastructure
 
         private void EnterLoadLevel()
         {
-            _stateMachine.Enter<LoadLevelState>();
+            _stateMachine.Enter<LoadLevelState, string>("Main");
         }
 
         private void RegisterServices()
--- Assets/CodeBase/Infrastructure/LoadLevelState.cs
+++ Assets/CodeBase/Infrastructure/LoadLevelState.cs
@@ -1,6 +1,6 @@
 ﻿namespace CodeBase.Infrastructure
 {
-    public class LoadLevelState : IState
+    public class LoadLevelState : IPayloadedState<string>
     {
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
@@ -11,9 +11,9 @@
             _sceneLoader = sceneLoader;
         }
 
-        public void Enter()
+        public void Enter(string sceneName)
         {
-           _sceneLoader.Load("Main"); 
+           _sceneLoader.Load(sceneName); 
         }
 
         public void Exit()
