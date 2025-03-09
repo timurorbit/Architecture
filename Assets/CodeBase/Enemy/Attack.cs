ba2372df2beeb258f532e0ccbd014d62a36f61f3
Timur <t.shamilov.hollow@gmail.com>
2025-03-09 13:37:16 +0100

    Enemy Attack Pattern
    Implementation with Component-based approach

---
+++ Assets/CodeBase/Enemy/Attack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services;
+using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class Attack : MonoBehaviour
+    {
+        public EnemyAnimator Animator;
+
+        public float AttackCooldown = 3f;
+        public float Clevage = 0.5f;
+        private float EffectiveDistance = 0.5f;
+
+        private IGameFactory _factory;
+        private Transform _heroTransform;
+        private float _attackCooldown;
+        private bool _isAttacking;
+        private int _layerMask;
+        private Collider[] _hits = new Collider[1];
+        private bool _attackIsActive;
+
+        private void Awake()
+        {
+            _factory = AllServices.Container.Single<IGameFactory>();
+            _factory.HeroCreated += OnHeroCreated;
+            
+            _layerMask = 1 << LayerMask.NameToLayer("Player");
+        }
+
+        private void Update()
+        {
+            UpdateCooldown();
+            
+            if (CanAttack())
+            {
+                StartAttack();
+            }
+        }
+
+        private void OnAttack()
+        {
+            if (Hit(out Collider hit))
+            {
+                PhysicsDebug.DrawDebug(StartPoint(), Clevage, 1);
+            }
+        }
+
+        private void UpdateCooldown()
+        {
+            if (!CooldownIsUp())
+            {
+                _attackCooldown -= Time.deltaTime;
+            }
+        }
+
+        private bool Hit(out Collider hit)
+        {
+            var hitCount = Physics.OverlapSphereNonAlloc(StartPoint(), Clevage, _hits, _layerMask);
+            hit = _hits.FirstOrDefault();
+            
+            return hitCount > 0;
+        }
+
+        private Vector3 StartPoint()
+        {
+            return new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z) + transform.forward * EffectiveDistance;
+        }
+
+        private void OnAttackEnded()
+        {
+            _attackCooldown = AttackCooldown;
+            _isAttacking = false;
+        }
+
+        private bool CooldownIsUp() => _attackCooldown <= 0;
+
+        private bool CanAttack() => _attackIsActive && !_isAttacking && CooldownIsUp();
+
+        private void StartAttack()
+        {
+            transform.LookAt(_heroTransform);
+            Animator.PlayAttack();
+
+            _isAttacking = true;
+        }
+
+        private void OnHeroCreated() =>
+            _heroTransform = _factory.HeroGameObject.transform;
+
+        public void DisableAttack() => _attackIsActive = false;
+
+        public void EnableAttack() => _attackIsActive = true;
+    }
+}
